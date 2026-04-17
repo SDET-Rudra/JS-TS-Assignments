bedbc4a591bf2a4417cdbeebeb4b9db50e806e6e
SDET-Rudra <rudra4testing@gmail.com>
2026-04-17 12:29:38 +0530

    submitting Assignment-1 on datatypes

---
+++ Assignments/assignment-1-data-types.ts
@@ -0,0 +1,95 @@
+//1. Temperature of a city in degrees Celsius: 25.5
+let temperatureCelsius: number = 25.5;
+console.log(`Temperature of a city in degrees ${temperatureCelsius} Celsius `);
+
+//2. Whether a customer has placed an order: true or false
+let hasPlacedOrder: boolean = true;
+console.log(`customer has placed an order: ${hasPlacedOrder}`);
+
+let hasPlacedOrder1: String = "true or false";
+console.log(`customer has placed an order: ${hasPlacedOrder1}`);
+
+let hasPlacedOrder2: boolean[] = [true, false];
+console.log(`customer has placed an order: ${hasPlacedOrder2[0]} or ${hasPlacedOrder2[1]}`);
+
+//3. Person's phone number: "123-456-7890"
+let phoneNumber: string = '"123-456-7890"';
+console.log(`Person's phone number: ${phoneNumber}`);
+
+//4. Amount of money in a customer's bank account: 1000.50
+let amout:number = 1000.50;
+console.log(`Amount of money in a customer's bank account: ${amout}`);
+
+//5. Person's email address: "john.doe@example.com"
+let emailAddress:String = '"john.doe@example.com"';
+console.log(`Person's email address: ${emailAddress}`);
+
+//6. Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+let location: number[] = [37.7749, -122.4194];
+console.log(`Coordinates of a location (latitude, longitude): ${location[0]}, ${location[1]}`);
+
+//7. Person's marital status: true or false
+let isMarried:boolean =false;
+console.log(`Person's marital status: ${isMarried}`);
+
+let isMarried1: String = "true or false";
+console.log(`Person's marital status: ${isMarried1}`);
+
+let isMarried2: boolean[] = [true, false];
+console.log(`Person's marital status: ${isMarried2[0]} or ${isMarried2[1]}`);
+
+//Person's occupation: "Software Engineer"
+let occupation:String = '"Software Engineer"';
+console.log(`Person's occupation: ${occupation}`);
+
+//9. Person's favourite colour: "Blue"
+let colour:String = '"Blue"';
+console.log(`Person's favourite colour: ${colour}`);
+
+//10.Current year: 2023
+let year:number = 2023;
+console.log(`Current year: ${year}`);
+
+let today= new Date();
+const currentYear= today.getFullYear();
+console.log(`Current year: ${currentYear}`);
+
+//11.Number of followers on a social media platform: 1,000,000
+let followers:String = "1,000,000";
+console.log(`Number of followers on a social media platform: ${followers}`);
+
+//12.Rating of a movie: 7.5
+let rating:number= 7.5;
+console.log(`Rating of a movie: ${rating}`);
+
+//13.Person's blood type: 'A'
+let type:String= "'A'";
+console.log(`Person's blood type: ${type}`);
+
+//14.Title of a book: "To Kill a Mockingbird"
+let bookTitle:String= '"To Kill a Mockingbird"';
+console.log(`Title of a book: ${bookTitle}`);
+
+//5.Number of employees in a company: 500
+let empNum:number= 500;
+console.log(`Number of employees in a company: ${empNum}`);
+
+//16.Time of an event: 2:30 PM
+let eventTime:String= "2:30 PM";
+console.log(`Time of an event: ${eventTime}`);
+
+//17.Name of a country: "United States"
+let countryName:String= '"United States"';
+console.log(`Name of a country: ${countryName}`);
+
+//18.Person's eye color: "Brown"
+let eyeColor:string= '"Brown"';
+console.log(`Person's eye color: ${eyeColor}`);
+
+//19.Person's birthplace: "New York City"
+let place:string= '"New York City"';
+console.log(`Person's birthplace: ${place}`);
+
+//20. Distance between two cities: 200.5
+let distance:number= 200.5
+console.log(`Distance between two cities: ${distance}`);
